c549c3c32cd26ac368e96edaddb60535f8f70fc9
Elaine-lin-2 <81188632+Elaine-lin-2@users.noreply.github.com>
2021-05-19 20:30:00 -0400

    Add files via upload

---
+++ FitnessCalculatorCS.java
@@ -0,0 +1,13 @@
+/*
+* Date: May 19th 2021
+* Names: Tiffany, Krishna, Marc-Anthoney, Elaine
+* Teacher: Mr.Ho
+* Description: 
+*
+*/
+
+class FitnessCalculatorCS{
+    public static void main(String[]args){
+        
+    }
+}
